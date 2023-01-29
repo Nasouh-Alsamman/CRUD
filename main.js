b1833f57a8f5b9bfe0bcc7155b1774b80d6dd40d
NasouhAlsamman <nasouhe@gmail.com>
2023-01-29 23:46:55 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,261 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+let mood = 'create';
+let tmp;
+
+function gettotal()
+{
+    if(price.value != '')
+    {
+    let result = ( +price.value + +taxes.value + +ads.value )- +discount.value;
+    total.innerHTML=result;
+    total.style.background ='#040'
+    }
+    else{
+        total.innerHTML='';
+        total.style.background ='rgb(95, 1, 1)'; 
+        taxes.value='';
+        ads.value='';
+        discount.value='';
+    }
+};
+
+
+let datapro;
+if(localStorage.product != null)    {
+datapro = JSON.parse(localStorage.product);
+}
+else{
+datapro = [];
+}
+
+submit.onclick = function(){
+    let newpro = {
+        title:title.value.toLowerCase(),
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value.toLowerCase(),
+    };
+
+
+        if(title.value != '' && price.value != '' && category.value != '' && newpro.count <= 100 )
+        {
+
+            if(mood === 'create')
+            {
+                if(newpro.count > 1)
+                                {
+                                    for(let i=0 ; i < newpro.count ; i++)
+                                        {
+                                            datapro.push(newpro);
+                                        }
+                                }
+                                else
+                                {
+                                    datapro.push(newpro);
+
+                                }
+            }
+                
+            else
+            {
+ 
+                datapro[ tmp ]= newpro;
+                mood='create'; 
+                submit.innerHTML='Create';
+                count.style.display='block';
+                submit.style.background='rgb(78, 78, 78)';
+            }
+
+                localStorage.setItem('product',JSON.stringify(datapro));
+                cleadata();
+                showdata();
+
+        }
+        else
+            {
+                alert("Sorry...you have to fill all field");  
+            }
+  
+
+
+};
+
+function cleadata(){
+    title.value='';
+    price.value='';
+    taxes.value='';
+    ads.value='';
+    discount.value='';
+    total.innerHTML='';
+    count.value='';
+    category.value='';
+
+};
+function showdata()
+{
+gettotal();
+    let table='';
+    for(let i=0 ; i < datapro.length ; i++){
+        table +=`
+        <tr>
+            <td>${i+1}</td>
+            <td>${datapro[i].title}</td>
+            <td>${datapro[i].price}</td>
+            <td>${datapro[i].taxes}</td>
+            <td>${datapro[i].ads}</td>
+            <td>${datapro[i].discount}</td>
+            <td>${datapro[i].total}</td>
+            <td>${datapro[i].category}</td>
+            <td><button onclick="update(${i})" id="updat">updat</button></td>
+            <td><button onclick="deletedata(${i})" id="delete">delete</button></td>
+        </tr>
+        `;
+    }
+    document.getElementById('tbody').innerHTML = table;
+
+   let btndelete=document.getElementById('deleteall');
+    if(datapro.length > 0){
+        btndelete.innerHTML=`
+        <button onclick="deletealldata()" id="hi">Delete All (${datapro.length})</button>
+        `
+    }
+    else{
+        btndelete.innerHTML='';
+    }
+}
+showdata();
+
+function deletedata (i){
+datapro.splice(i,1);
+localStorage.product=JSON.stringify(datapro);
+showdata();
+}
+
+function deletealldata (){
+
+localStorage.clear();
+datapro.splice(0);
+showdata();
+
+}
+
+function update(i){
+title.value=datapro[i].title;
+price.value=datapro[i].price;
+taxes.value=datapro[i].taxes;
+ads.value=datapro[i].ads;
+discount.value=datapro[i].discount;
+gettotal();
+count.style.display='none';
+category.value=datapro[i].category;
+submit.innerHTML='Update';
+submit.style.background='rgb(51, 34, 146)';
+mood ='update';
+tmp = i ;
+scroll({
+    top:0,
+    behavior:"smooth", 
+
+})
+
+}
+
+let searchmood='title';
+
+function getsearchmood(id){
+ let search=document.getElementById('search');
+ search.classList.remove('hide');
+
+  if(id == 'searchtitle'){
+    searchmood='title';
+    search.placeholder='Search By Title';
+  }
+  else{
+    searchmood='category';
+    search.placeholder='Search By Category';
+
+  }
+
+  console.log(searchmood);
+  search.focus();
+  search.value='';
+  showdata();
+
+
+}
+
+
+
+function searchdata(value){
+    let table='';
+if(searchmood=='title')
+{
+    for(let i=0 ; i < datapro.length ; i++){
+        if(datapro[i].title.includes(value.toLowerCase())){
+           
+            table +=`
+            <tr>
+                <td>${i}</td>
+                <td>${datapro[i].title}</td>
+                <td>${datapro[i].price}</td>
+                <td>${datapro[i].taxes}</td>
+                <td>${datapro[i].ads}</td>
+                <td>${datapro[i].discount}</td>
+                <td>${datapro[i].total}</td>
+                <td>${datapro[i].category}</td>
+                <td><button onclick="update(${i})" id="updat">updat</button></td>
+                <td><button onclick="deletedata(${i})" id="delete">delete</button></td>
+            </tr>
+            `;
+
+
+        }
+    }
+
+} else{
+    for(let i=0 ; i < datapro.length ; i++){
+        if(datapro[i].category.includes(value.toLowerCase())){
+           
+            table +=`
+            <tr>
+                <td>${i}</td>
+                <td>${datapro[i].title}</td>
+                <td>${datapro[i].price}</td>
+                <td>${datapro[i].taxes}</td>
+                <td>${datapro[i].ads}</td>
+                <td>${datapro[i].discount}</td>
+                <td>${datapro[i].total}</td>
+                <td>${datapro[i].category}</td>
+                <td><button onclick="update(${i})" id="updat">updat</button></td>
+                <td><button onclick="deletedata(${i})" id="delete">delete</button></td>
+            </tr>
+            `;
+
+
+        }
+    }
+
+}
+
+
+document.getElementById('tbody').innerHTML = table;
+
+
+}
+function hide(){
+    search.classList.add('hide');
+
+}
